be8147b5776c685462bc3954d574b6c3302f97a2
Giovanni Toraldo <me@gionn.net>
2014-04-22 23:20:55 +0200

    Add build-essential dependency

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,6 @@ if platform_family?('debian')
 	include_recipe 'apt'
 end
 
-%w{automake autoconf libcurl4-gnutls-dev}.each do |pkg|
+%w{build-essential automake autoconf libcurl4-gnutls-dev}.each do |pkg|
 	package pkg
 end
